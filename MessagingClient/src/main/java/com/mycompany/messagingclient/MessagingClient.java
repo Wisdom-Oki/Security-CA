0e929514f4de4b967712b40a2308426e577186cf
TomatechGames <theludwiggamer@gmail.com>
2023-11-18 20:31:01 +0000

    Renamed some stuff

---
+++ MessagingClient/src/main/java/com/mycompany/messagingclient/MessagingClient.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.messagingclient;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author wisdom
+ */
+public class MessagingClient {
+    private static InetAddress host;
+    private static final int PORT = 1234;
+    private static String secretKey;
+    public static String clientMessage;
+
+    public static void main(String[] args) {
+        MessagingClientGUI.StartGUI();
+        try {
+            host = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        }
+        run();
+    }
+
+    private static void run() {
+        Socket link = null; // Step 1.
+        try {
+            link = new Socket(host, PORT); // Step 1.
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));// Step 2.
+            PrintWriter out = new PrintWriter(link.getOutputStream(), true); // Step 2.
+
+            // Set up stream for keyboard entry...
+            //BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
+
+            MessagingClientGUI.AppendToOutput("Enter Link Code: ");
+            
+            clientMessage = null;
+            while (clientMessage==null) {
+                Thread.sleep(100);
+            }
+            MessagingClientGUI.AppendToOutputLn(clientMessage);
+            out.println(clientMessage);
+            MessagingClientGUI.AppendToOutputLn("Waiting For Connection...");
+            secretKey = in.readLine();
+            MessagingClientGUI.AppendToOutputLn("Connection Established!");
+
+            while (true) {
+                MessagingClientGUI.AppendToOutput("You         > ");
+
+                clientMessage = null;
+                while (clientMessage==null) {
+                    Thread.sleep(100);
+                }
+                MessagingClientGUI.AppendToOutputLn(clientMessage);
+                if (clientMessage.startsWith("/")) {
+                    out.println(clientMessage);
+                } else {
+                    out.println(Encryption.encrypt(clientMessage, secretKey));
+                }
+
+                String recievedMessage = in.readLine();
+                if (recievedMessage!=null && recievedMessage.startsWith("/")) {
+                    String[] splitMessage = recievedMessage.substring(1).split(" ");
+                    if (splitMessage[0].equals("TERMINATE")) {
+                        MessagingClientGUI.AppendToOutput("Server terminated connection.");
+                        if (splitMessage.length > 1) {
+                            MessagingClientGUI.AppendToOutput(" Reason: \"" + splitMessage[1].replace("_", " ")+"\"");
+                        }
+                        MessagingClientGUI.AppendToOutputLn("");
+                        break;
+                    }
+                } else {
+                    MessagingClientGUI.AppendToOutputLn("Remote > " + Encryption.decrypt(recievedMessage, secretKey));
+                }
+            }
+
+            // send link code to server
+            // wait for encryption key from server
+            // store key, use to encrypt all future messages and decrypt all future messages
+            // and responses
+        } catch (IOException e) {
+            //nono
+            //e.printStackTrace();
+        } catch (InterruptedException e) {
+            //nono
+            //e.printStackTrace();
+        }finally {
+            try {
+                System.out.println("\n* Closing connection... *");
+                link.close(); // Step 4.
+            } catch (IOException e) {
+                System.out.println("Unable to disconnect/close!");
+                System.exit(1);
+            }
+        }
+    } // finish run method
+} // finish the class
---
+++ MessagingServer/src/main/java/com/mycompany/messagingserver/MessagingServer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.messagingserver;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author
+ */
+public class MessagingServer {
+  private static ServerSocket servSock;
+  private static final int PORT = 1234;
+  private static int clientConnections = 0;
+
+  public static void main(String[] args) {
+    System.out.println("Opening port...\n");
+    try {
+      servSock = new ServerSocket(PORT); // Step 1.
+    } catch (IOException e) {
+      System.out.println("Unable to attach to port!");
+      System.exit(1);
+    }
+
+    do {
+      run();
+    } while (true);
+
+  }
+
+  private static void run() {
+    Socket link = null; // Step 2.
+    try {
+      link = servSock.accept();
+      clientConnections++;
+      String client_ID = "Client " + clientConnections;
+      Runnable resource = new ClientConnectionRun(link, client_ID);
+      Thread t = new Thread(resource);
+      t.start();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+      try {
+        System.out.println("\n* Closing connection... *");
+        link.close(); // Step 5.
+      } catch (IOException e2) {
+        System.out.println("Unable to disconnect!");
+        System.exit(1);
+      }
+    }
+  } // finish run method
+} // finish the class
