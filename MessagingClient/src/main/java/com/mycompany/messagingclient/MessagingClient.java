21479d0b057ccacdd2e866dd2bae11d7ddaabd2f
Eve Gibney <gibney.eg@gmail.com>
2023-11-18 20:56:58 +0000

    Added how to stop connection
    Added a print line telling user how to stop the connection. got rid of various useless lines.

--- MessagingClient/src/main/java/com/mycompany/messagingclient/MessagingClient.java
+++ MessagingClient/src/main/java/com/mycompany/messagingclient/MessagingClient.java
@@ -30,14 +30,13 @@ public class MessagingClient {
     }
 
     private static void run() {
-        Socket link = null; // Step 1.
+        Socket link = null; 
         try {
-            link = new Socket(host, PORT); // Step 1.
-            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));// Step 2.
-            PrintWriter out = new PrintWriter(link.getOutputStream(), true); // Step 2.
+            link = new Socket(host, PORT); 
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            PrintWriter out = new PrintWriter(link.getOutputStream(), true); 
 
-            // Set up stream for keyboard entry...
-            //BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
+
 
             MessagingClientGUI.AppendToOutput("Enter Link Code: ");
             
@@ -45,12 +44,15 @@ public class MessagingClient {
             while (clientMessage==null) {
                 Thread.sleep(100);
             }
+            
             MessagingClientGUI.AppendToOutputLn(clientMessage);
             out.println(clientMessage);
             MessagingClientGUI.AppendToOutputLn("Waiting For Connection...");
             secretKey = in.readLine();
             MessagingClientGUI.AppendToOutputLn("Connection Established!");
+            MessagingClientGUI.AppendToOutputLn("To disconnect from server, type /STOP ");
 
+            
             while (true) {
                 MessagingClientGUI.AppendToOutput("You         > ");
 
@@ -81,24 +83,11 @@ public class MessagingClient {
                 }
             }
 
-            // send link code to server
-            // wait for encryption key from server
-            // store key, use to encrypt all future messages and decrypt all future messages
-            // and responses
         } catch (IOException e) {
-            //nono
-            //e.printStackTrace();
+
         } catch (InterruptedException e) {
-            //nono
-            //e.printStackTrace();
-        }finally {
-            try {
-                System.out.println("\n* Closing connection... *");
-                link.close(); // Step 4.
-            } catch (IOException e) {
-                System.out.println("Unable to disconnect/close!");
-                System.exit(1);
-            }
+
+            
         }
-    } // finish run method
-} // finish the class
+    } 
+} 
--- MessagingServer/src/main/java/com/mycompany/messagingserver/MessagingServer.java
+++ MessagingServer/src/main/java/com/mycompany/messagingserver/MessagingServer.java
@@ -13,43 +13,37 @@ import java.net.*;
  * @author
  */
 public class MessagingServer {
-  private static ServerSocket servSock;
-  private static final int PORT = 1234;
-  private static int clientConnections = 0;
 
-  public static void main(String[] args) {
-    System.out.println("Opening port...\n");
-    try {
-      servSock = new ServerSocket(PORT); // Step 1.
-    } catch (IOException e) {
-      System.out.println("Unable to attach to port!");
-      System.exit(1);
+    private static ServerSocket servSock;
+    private static final int PORT = 1234;
+    private static int clientConnections = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Opening port...\n");
+        try {
+            servSock = new ServerSocket(PORT); 
+        } catch (IOException e) {
+            System.out.println("Unable to attach to port!");
+            System.exit(1);
+        }
+
+        do {
+            run();
+        } while (true);
+
     }
 
-    do {
-      run();
-    } while (true);
+    private static void run() {
+        Socket link; 
+        try {
+            link = servSock.accept();
+            clientConnections++;
+            String client_ID = "Client " + clientConnections;
+            Runnable resource = new ClientConnectionRun(link, client_ID);
+            Thread t = new Thread(resource);
+            t.start();
+        } catch (IOException e1) {
 
-  }
-
-  private static void run() {
-    Socket link = null; // Step 2.
-    try {
-      link = servSock.accept();
-      clientConnections++;
-      String client_ID = "Client " + clientConnections;
-      Runnable resource = new ClientConnectionRun(link, client_ID);
-      Thread t = new Thread(resource);
-      t.start();
-    } catch (IOException e1) {
-      e1.printStackTrace();
-      try {
-        System.out.println("\n* Closing connection... *");
-        link.close(); // Step 5.
-      } catch (IOException e2) {
-        System.out.println("Unable to disconnect!");
-        System.exit(1);
-      }
-    }
-  } // finish run method
-} // finish the class
+        }
+    } 
+} 
